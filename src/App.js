d120e26516613b32bd8f6fb316d83d845c719021
Nipa2345 <patelnipaj74@gmail.com>
2022-03-06 20:11:18 -0500

    setup web app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Covid from "./components/covid";
+//import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Covid/>
+    </>
+  )
+}
+export default App
+
